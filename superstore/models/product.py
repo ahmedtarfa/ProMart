51da5d7601b33027d9ba439e53b14542199c628c
Ahmed <ahmedshtarfa@gmail.com>
2025-05-29 00:19:51 +0300

    remove the superstore folder

--- superstore/models/product.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class SuperstoreProduct(models.Model):
-    _inherit = "product.template"
-
-    x_productid=fields.Char(string="Product ID", required=False, default='N/A')
-    x_sub_category=fields.Char(string='Sub Category2', required=False, default='N/A')
-
